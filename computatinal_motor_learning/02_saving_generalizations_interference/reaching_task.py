5de8665784e38e815f1696fe0b939b85477c575f
Ahmed Eldably <ahmed.h.eldably@gmail.com>
2025-01-22 19:15:09 +0100

    Adding argument variables

--- computatinal_motor_learning/02_saving_generalizations_interference/reaching_task.py
+++ computatinal_motor_learning/02_saving_generalizations_interference/reaching_task.py
@@ -1,39 +1,44 @@
+import argparse
+
 import pygame
 import random
 import math
 import numpy as np
 import pandas as pd
 import os
+from task_config import TaskConfig
+
 
 # ========================== EXPERIMENT CONFIGURATION ==========================
-class Config:
-    SCREEN_X = int(os.getenv('SCREEN_X', 3840))  # Screen resolution X
-    SCREEN_Y = int(os.getenv('SCREEN_Y', 2160))  # Screen resolution Y
-    WIDTH, HEIGHT = SCREEN_X // 1.5, SCREEN_Y // 1.5  # Adjust for scaling
-    CIRCLE_SIZE = 20
-    TARGET_SIZE = CIRCLE_SIZE
-    TARGET_RADIUS = 300
-    MASK_RADIUS = 0.66 * TARGET_RADIUS
-    ATTEMPTS_LIMIT = 200
-    TIME_LIMIT = 1000  # Time limit per attempt (ms)
-
-    # Start Positions
-    START_POSITION = (WIDTH // 2, HEIGHT // 2)
-    START_ANGLE = 0
-    PERTURBATION_ANGLE = math.radians(30)  # Perturbation Angle in Radians
-
-    # Colors
-    WHITE, BLACK, RED, BLUE = (255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 0, 255)
+# class self.config:
+#     SCREEN_X = int(os.getenv('SCREEN_X', 3840))  # Screen resolution X
+#     SCREEN_Y = int(os.getenv('SCREEN_Y', 2160))  # Screen resolution Y
+#     WIDTH, HEIGHT = SCREEN_X // 1.5, SCREEN_Y // 1.5  # Adjust for scaling
+#     CIRCLE_SIZE = 20
+#     TARGET_SIZE = CIRCLE_SIZE
+#     TARGET_RADIUS = 300
+#     MASK_RADIUS = 0.66 * TARGET_RADIUS
+#     ATTEMPTS_LIMIT = 200
+#     TIME_LIMIT = 1000  # Time limit per attempt (ms)
+#
+#     # Start Positions
+#     START_POSITION = (WIDTH // 2, HEIGHT // 2)
+#     START_ANGLE = 0
+#     PERTURBATION_ANGLE = math.radians(30)  # Perturbation Angle in Radians
+#
+#     # Colors
+#     WHITE, BLACK, RED, BLUE = (255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 0, 255)
 
 # ========================== PARTICIPANT DATA HANDLING ==========================
 class Participant:
-    def __init__(self, participant_id, age, gender, handedness):
+    def __init__(self, participant_id, age, gender, handedness, task_config):
         self.id = participant_id
         self.age = age
         self.gender = gender
         self.handedness = handedness
         self.session = self.get_next_session_number()
         self.trial_data = []  # Stores all trials
+        self.task_config = task_config
 
     def get_next_session_number(self):
         """Determine the next session number for the participant to avoid overwriting."""
@@ -57,7 +62,8 @@ class Participant:
         df = pd.DataFrame(self.trial_data)
 
         # Define directory and filename
-        directory = f'participant_data/{self.id}'
+        directory_location = f'participant_data/{self.id}/{self.task_config.experiment_name}/{self.task_config.phase}'
+        directory = os.path.join(os.path.dirname(__file__), f'{directory_location}')
         filename = f'{directory}/participant_{self.id}_session_{self.session}.csv'
 
         # Ensure the directory exists
@@ -70,10 +76,11 @@ class Participant:
 
 # ========================== EXPERIMENT CLASS ==========================
 class MotorLearningExperiment:
-    def __init__(self, participant):
+    def __init__(self, participant, task_config):
+        self.config = task_config
         self.error_angles = []
         pygame.init()
-        self.screen = pygame.display.set_mode((Config.WIDTH, Config.HEIGHT), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((self.config.WIDTH, self.config.HEIGHT), pygame.FULLSCREEN)
         pygame.display.set_caption("Reaching Game")
         self.clock = pygame.time.Clock()
 
@@ -103,10 +110,10 @@ class MotorLearningExperiment:
     # ========================== HELPER FUNCTIONS ==========================
     def generate_target_position(self):
         """Generate a new target position based on the selected target mode."""
-        angle = random.uniform(0, 2 * math.pi) if self.target_mode == 'random' else math.radians(Config.START_ANGLE)
+        angle = random.uniform(0, 2 * math.pi) if self.target_mode == 'random' else math.radians(self.config.START_ANGLE)
         return [
-            Config.WIDTH // 2 + Config.TARGET_RADIUS * math.sin(angle),
-            Config.HEIGHT // 2 + Config.TARGET_RADIUS * -math.cos(angle)
+            self.config.WIDTH // 2 + self.config.TARGET_RADIUS * math.sin(angle),
+            self.config.HEIGHT // 2 + self.config.TARGET_RADIUS * -math.cos(angle)
         ]
 
     def update_experiment_design(self):
@@ -129,12 +136,12 @@ class MotorLearningExperiment:
         if self.new_target:
             distance = math.hypot(circle_pos[0] - self.new_target[0],
                                              circle_pos[1] - self.new_target[1])
-            return distance <= Config.CIRCLE_SIZE
+            return distance <= self.config.CIRCLE_SIZE
         return False
 
     def at_start_position_and_generate_target(self, mouse_pos):
-        distance = math.hypot(mouse_pos[0] - Config.START_POSITION[0], mouse_pos[1] - Config.START_POSITION[1])
-        if distance <= Config.CIRCLE_SIZE:
+        distance = math.hypot(mouse_pos[0] - self.config.START_POSITION[0], mouse_pos[1] - self.config.START_POSITION[1])
+        if distance <= self.config.CIRCLE_SIZE:
             return True
         return False
 
@@ -152,9 +159,9 @@ class MotorLearningExperiment:
         """Calculate perturbed mouse position based on perturbation type (same as original code)."""
         if self.perturbation_mode:
             if self.perturbation_type == 'sudden':
-                perturbed_mouse_angle = mouse_angle + Config.PERTURBATION_ANGLE
+                perturbed_mouse_angle = mouse_angle + self.config.PERTURBATION_ANGLE
             elif self.perturbation_type == 'gradual':
-                perturbed_mouse_angle = mouse_angle - self.gradual_step * (Config.PERTURBATION_ANGLE / 10)
+                perturbed_mouse_angle = mouse_angle - self.gradual_step * (self.config.PERTURBATION_ANGLE / 10)
 
                 if self.gradual_attempts % 3 == 0 and self.prev_gradual_attempts != self.gradual_attempts:
                     self.gradual_step += 1
@@ -163,8 +170,8 @@ class MotorLearningExperiment:
                     self.gradual_attempts = 0  # Reset after 10 steps for cyclic perturbation
 
             return [
-                Config.START_POSITION[0] + distance * math.cos(perturbed_mouse_angle),
-                Config.START_POSITION[1] + distance * math.sin(perturbed_mouse_angle)
+                self.config.START_POSITION[0] + distance * math.cos(perturbed_mouse_angle),
+                self.config.START_POSITION[1] + distance * math.sin(perturbed_mouse_angle)
             ]
 
         return pygame.mouse.get_pos()  # No perturbation
@@ -175,7 +182,7 @@ class MotorLearningExperiment:
         running = True
 
         while running:
-            self.screen.fill(Config.BLACK)
+            self.screen.fill(self.config.BLACK)
 
             # ========================== EVENT HANDLING ==========================
             for event in pygame.event.get():
@@ -196,7 +203,7 @@ class MotorLearningExperiment:
             self.update_experiment_design()
 
             # Stop experiment when reaching attempt limit
-            if self.attempts > Config.ATTEMPTS_LIMIT:
+            if self.attempts > self.config.ATTEMPTS_LIMIT:
                 running = False
 
             # Hide the mouse cursor
@@ -204,15 +211,15 @@ class MotorLearningExperiment:
 
             # ========================== TRACK MOUSE MOVEMENT ==========================
             mouse_pos = pygame.mouse.get_pos()
-            deltax, deltay = mouse_pos[0] - Config.START_POSITION[0], mouse_pos[1] - Config.START_POSITION[1]
+            deltax, deltay = mouse_pos[0] - self.config.START_POSITION[0], mouse_pos[1] - self.config.START_POSITION[1]
             distance, mouse_angle = math.hypot(deltax, deltay), math.atan2(deltay, deltax)
 
             # ========================== CALCULATE PERTURBATION ==========================
             if self.perturbation_mode:
                 if self.perturbation_type == 'sudden':
-                    perturbed_mouse_angle = mouse_angle + Config.PERTURBATION_ANGLE
+                    perturbed_mouse_angle = mouse_angle + self.config.PERTURBATION_ANGLE
                 elif self.perturbation_type == 'gradual':
-                    perturbed_mouse_angle = mouse_angle - self.gradual_step * (Config.PERTURBATION_ANGLE / 10)
+                    perturbed_mouse_angle = mouse_angle - self.gradual_step * (self.config.PERTURBATION_ANGLE / 10)
 
                     if self.gradual_attempts % 3 == 0 and self.prev_gradual_attempts != self.gradual_attempts:
                         self.gradual_step += 1
@@ -221,8 +228,8 @@ class MotorLearningExperiment:
                         self.gradual_attempts = 0  # Reset after 10 steps for cyclic perturbation
 
                 circle_pos = [
-                    Config.START_POSITION[0] + distance * math.cos(perturbed_mouse_angle),
-                    Config.START_POSITION[1] + distance * math.sin(perturbed_mouse_angle)
+                    self.config.START_POSITION[0] + distance * math.cos(perturbed_mouse_angle),
+                    self.config.START_POSITION[1] + distance * math.sin(perturbed_mouse_angle)
                 ]
             else:
                 circle_pos = pygame.mouse.get_pos()
@@ -235,7 +242,7 @@ class MotorLearningExperiment:
                 self.attempts += 1
                 hit = True
 
-                error_angle = self.compute_error_angle(Config.START_POSITION, self.new_target, circle_pos)
+                error_angle = self.compute_error_angle(self.config.START_POSITION, self.new_target, circle_pos)
                 if error_angle is not None:
                     self.participant.log_trial(self.attempts, error_angle, hit)
 
@@ -245,12 +252,12 @@ class MotorLearningExperiment:
                     self.gradual_attempts += 1
 
             # miss if player leaves the target_radius + 1% tolerance
-            elif self.new_target and math.hypot(circle_pos[0] - Config.START_POSITION[0],
-                                                circle_pos[1] - Config.START_POSITION[1]) > Config.TARGET_RADIUS * 1.01:
+            elif self.new_target and math.hypot(circle_pos[0] - self.config.START_POSITION[0],
+                                                circle_pos[1] - self.config.START_POSITION[1]) > self.config.TARGET_RADIUS * 1.01:
                 self.attempts += 1
                 hit = False
 
-                error_angle = self.compute_error_angle(Config.START_POSITION, self.new_target, circle_pos)
+                error_angle = self.compute_error_angle(self.config.START_POSITION, self.new_target, circle_pos)
                 if error_angle is not None:
                     self.participant.log_trial(self.attempts, error_angle, hit)
 
@@ -267,15 +274,15 @@ class MotorLearningExperiment:
                 self.start_time = pygame.time.get_ticks()
 
             # ========================== TIME LIMIT CHECK ==========================
-            if self.start_time and (pygame.time.get_ticks() - self.start_time) > Config.TIME_LIMIT:
+            if self.start_time and (pygame.time.get_ticks() - self.start_time) > self.config.TIME_LIMIT:
                 self.move_faster = True
                 self.start_time = 0  # Reset timer
 
             # ========================== DISPLAY 'MOVE FASTER' WARNING ==========================
             if self.move_faster:
                 font = pygame.font.Font(None, 36)
-                text = font.render('MOVE FASTER!', True, Config.RED)
-                text_rect = text.get_rect(center=Config.START_POSITION)
+                text = font.render('MOVE FASTER!', True, self.config.RED)
+                text_rect = text.get_rect(center=self.config.START_POSITION)
                 self.screen.blit(text, text_rect)
                 # Exclude attempts
                 if len(self.error_angles) > 0:
@@ -284,34 +291,31 @@ class MotorLearningExperiment:
 
             # ========================== DRAW GAME ELEMENTS ==========================
             if self.new_target:
-                pygame.draw.circle(self.screen, Config.BLUE, self.new_target, Config.TARGET_SIZE // 2)
+                pygame.draw.circle(self.screen, self.config.BLUE, self.new_target, self.config.TARGET_SIZE // 2)
 
             if self.mask_mode:
-                if distance < Config.MASK_RADIUS:
-                    pygame.draw.circle(self.screen, Config.WHITE, circle_pos, Config.CIRCLE_SIZE // 2)
+                if distance < self.config.MASK_RADIUS:
+                    pygame.draw.circle(self.screen, self.config.WHITE, circle_pos, self.config.CIRCLE_SIZE // 2)
             else:
-                pygame.draw.circle(self.screen, Config.WHITE, circle_pos, Config.CIRCLE_SIZE // 2)
+                pygame.draw.circle(self.screen, self.config.WHITE, circle_pos, self.config.CIRCLE_SIZE // 2)
 
             # Draw start position
-            pygame.draw.circle(self.screen, Config.WHITE, Config.START_POSITION, 5)
+            pygame.draw.circle(self.screen, self.config.WHITE, self.config.START_POSITION, 5)
 
 
             # ========================== DISPLAY SCORE & ATTEMPTS ==========================
             font = pygame.font.Font(None, 36)
-            score_text = font.render(f"Score: {self.score}", True, Config.WHITE)
+            score_text = font.render(f"Score: {self.score}", True, self.config.WHITE)
             self.screen.blit(score_text, (10, 10))
 
-            score_text = font.render(f"mask_mode: {self.mask_mode}", True, Config.WHITE)
-            self.screen.blit(score_text, (10, 40))
-
-            attempts_text = font.render(f"Attempts: {self.attempts}", True, Config.WHITE)
+            attempts_text = font.render(f"Attempts: {self.attempts}", True, self.config.WHITE)
             self.screen.blit(attempts_text, (10, 30))
 
             # ========================== DEBUGGING INFO (IF TOGGLED) ==========================
             if self.show_mouse_info:
-                mouse_info_text = font.render(f"Mouse: x={mouse_pos[0]}, y={mouse_pos[1]}", True, Config.WHITE)
-                delta_info_text = font.render(f"Delta: Δx={deltax}, Δy={deltay}", True, Config.WHITE)
-                mouse_angle_text = font.render(f"Mouse_Ang: {np.rint(np.degrees(mouse_angle))}", True, Config.WHITE)
+                mouse_info_text = font.render(f"Mouse: x={mouse_pos[0]}, y={mouse_pos[1]}", True, self.config.WHITE)
+                delta_info_text = font.render(f"Delta: Δx={deltax}, Δy={deltay}", True, self.config.WHITE)
+                mouse_angle_text = font.render(f"Mouse_Ang: {np.rint(np.degrees(mouse_angle))}", True, self.config.WHITE)
                 self.screen.blit(mouse_info_text, (10, 60))
                 self.screen.blit(delta_info_text, (10, 90))
                 self.screen.blit(mouse_angle_text, (10, 120))
@@ -325,9 +329,20 @@ class MotorLearningExperiment:
 
 # ========================== RUN EXPERIMENT ==========================
 if __name__ == "__main__":
-    participant = Participant(participant_id=1,
-                              age=25,
-                              gender="M",
-                              handedness="Right")
-    experiment = MotorLearningExperiment(participant)
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description='Run Motor Learning Experiment')
+    parser.add_argument('--experiment_name', type=str, required=True, help='Name of the experiment')
+    parser.add_argument('--phase', type=int, required=True, help='Phase of the experiment')
+    args = parser.parse_args()
+
+    task_config = TaskConfig(args.experiment_name, args.phase)
+
+    participant = Participant(participant_id='Ka',
+                              age=23,
+                              gender="F",
+                              handedness="Right",
+                              task_config=task_config)
+    experiment = MotorLearningExperiment(participant,
+                                         task_config=task_config)
     experiment.run()
+
